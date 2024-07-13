ba81bb8ff72b6d4c39e51d647fb1f8b02580961a
vivek chaturvedi <vc8bp3@gmail.com>
2024-07-13 03:47:49 +0000

    changed ui

--- src/LineChart.jsx
+++ src/LineChart.jsx
@@ -1,58 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import ReactECharts from 'echarts-for-react';
-import { ratingToData, statusData } from './data';
-
-
-
+import React, { useState, useEffect } from "react";
+import ReactECharts from "echarts-for-react";
+import { ratingToData, statusData } from "./data";
 
 const LineChart = ({ finalData }) => {
-  const [data, setData] = useState({ labels: [], expressionData: [], });
-
+  const [data, setData] = useState({ labels: [], expressionData: [] });
 
   useEffect(() => {
     const labels = [];
     const expressionData = [];
 
     finalData.forEach((newData) => {
-        labels.push(newData.time)
-        expressionData.push(statusData[newData.expression].rating)
+      labels.push(newData.time);
+      expressionData.push(statusData[newData.expression].rating);
     });
 
-    setData({labels,expressionData})
+    setData({ labels, expressionData });
   }, [finalData]);
-
 
   const option = {
     xAxis: {
-      type: 'category',
+      type: "category",
       data: data.labels,
     },
     yAxis: [
       {
-        type: 'value',
-        name: 'Expression',
+        type: "value",
+        name: "Expression",
         scale: true,
       },
       {
-        type: 'value',
-        name: 'Probability',
+        type: "value",
+        name: "Probability",
         scale: true,
       },
     ],
     tooltip: {
-      trigger: 'axis',
+      trigger: "axis",
       axisPointer: {
-        type: 'shadow'
+        type: "shadow",
       },
       formatter: function (params) {
-        console.log({params})
+        console.log({ params });
         return ratingToData[params[0].value].emotion;
-      }
+      },
     },
     series: [
       {
-        name: 'Expression',
-        type: 'bar',
+        name: "Expression",
+        type: "bar",
         smooth: true,
         showSymbol: false,
         data: data.expressionData.map((value) => ({
@@ -62,38 +57,37 @@ const LineChart = ({ finalData }) => {
           },
           emphasis: {
             itemStyle: {
-              borderColor: 'transparent',
-            }
+              borderColor: "transparent",
+            },
           },
         })),
         label: {
           show: true,
-          position: 'inside',
-          formatter: d => ratingToData[d.value].emoji
+          position: "inside",
+          formatter: (d) => ratingToData[d.value].emoji,
         },
         yAxisIndex: 0,
-        
       },
     ],
     legend: {
-      data: ['Expression', 'Probability'], 
+      data: ["Expression", "Probability"],
     },
     dataZoom: [
       {
-        type: 'inside', 
+        type: "inside",
         xAxisIndex: [0],
       },
       {
-        type: 'slider',
-        xAxisIndex: [0], 
-        start: 0, 
-        end: 100, 
-        maxValueSpan: 10, 
+        type: "slider",
+        xAxisIndex: [0],
+        start: 0,
+        end: 100,
+        maxValueSpan: 10,
       },
     ],
   };
 
-  return <ReactECharts  option={option} />;
+  return <ReactECharts option={option} />;
 };
 
 export default LineChart;
